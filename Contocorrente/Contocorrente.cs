3f80721c50979f2d156d1dfa489f3007e67a53bf
SebastianoCastorina <134309028+SebastianoCastorina@users.noreply.github.com>
2023-08-30 16:50:42 +0200

    Add files via upload

---
+++ Contocorrente/Contocorrente.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contocorrente
+{
+    internal class Contocorrente
+    {
+        private string Nome { get; set; }
+        private string Cognome { get; set; }
+        public double Saldo { get; set; }
+
+        public bool ContoAperto = false;
+
+        public void AprireConto(double tot)
+        {
+            if (ContoAperto) { Console.WriteLine("Hai già un conto aperto!"); }
+            else
+            {
+                Saldo = 0;
+                ContoAperto = true;
+                if (tot >= 1000) Versamento(tot);
+                else  Console.WriteLine("Metti più soldi");
+            }
+        }
+        public double Versamento(double tot)
+        {
+            return Saldo += tot;
+        }
+        public double Prelievo(double tot)
+        {
+            return Saldo -= tot;
+        }
+        public string StatoDelConto()
+        {
+            return $"Il conto è intestato a {Nome} {Cognome}, un poveraccio, infatti il Saldo totale è {Saldo}";
+        }
+        public Contocorrente() { }
+        public Contocorrente (string Nome, string Cognome)
+        {
+            this.Nome = Nome;
+            this.Cognome = Cognome;
+        }
+    }
+
+
+}
